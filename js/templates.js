7f347d81c53d3cfb2061e9a67379673511592f1b
Jonathan Beurel <bejonster@gmail.com>
2014-11-13 00:24:51 +0100

    Update 2014-11-12T23:24:51.234Z

--- js/templates.js
+++ js/templates.js
@@ -1,1 +1,1 @@
-angular.module("app.templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("ranking.html","<h1>Ranking !</h1><ul><li ng-repeat=\"rider in riders\"><img src=\"{{ rider.profilePicture }}\">{{ rider.username }}</li></ul>");}]);
+angular.module("app.templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("ranking.html","<h1>Ranking</h1><ul><li ng-repeat=\"rider in riders\"><img src=\"{{ rider.profilePicture }}\">{{ rider.username }}</li></ul>");}]);
